9edecd62b0a3221bc2d94a12c618d51db1a2ae80
Heshani Rashmika <heshanirashmika29@gmail.com>
2025-03-25 13:43:45 +0530

    Structure

---
+++ src/main/java/com/example/user/customer.java
@@ -0,0 +1,23 @@
+package com.example.user;
+
+public class customer {
+    String name;
+    String email;
+    String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
